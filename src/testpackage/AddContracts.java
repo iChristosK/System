a23d7631b1a7d31635666a8bf142f70de090ef44
Christos <iChristos_@hotmail.com>
2017-01-29 23:55:29 +0200

    Contracts
    Table of Contracts

--- src/testpackage/AddContracts.java
+++ src/testpackage/AddContracts.java
@@ -198,10 +198,7 @@ public class AddContracts extends javax.swing.JFrame {
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
