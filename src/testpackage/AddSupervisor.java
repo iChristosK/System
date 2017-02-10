a874110ad83e746cb454ae1d5d4ffdb593dc3e0b
Christos <iChristos_@hotmail.com>
2017-02-10 21:56:12 +0200

    Supervisor
    Corrected

--- src/testpackage/AddSupervisor.java
+++ src/testpackage/AddSupervisor.java
@@ -603,100 +603,10 @@ public void executeSQlQuery(String query, String message)
         
         else {
             String query = "INSERT INTO `supervisors`(`ID`,`FullName`) VALUES ('"+jTextField1.getText()+"','"+jTextField2.getText()+"')";
+        
             executeSQlQuery(query, "Inserted");
         }
-    
-        
-        try {
-        
-        int rows = jTable2.getRowCount();
-        .forName("Class.forName("com.mysql.jdbc.Driver").newInstance();
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/kios",root,9667);
-        con.setAutoCommit(false);
-        
-       // String query =
-       //  String query = "INSERT INTO `supervisors`(`ID`,`FullName`) VALUES ('"+jTextField1.getText()+"','"+jTextField2.getText()+"')";
-        
-        // String query = "INSERT INTO `supervisors`(`ID`,`FullName`) VALUES (?,?)";
-        
-        for(int row = 0; row<rows; row++)
-{
-    String coitemname = (String)jTable2.getValueAt(row, 0);
-    String cocateg = (String)jTable2.getValueAt(row, 1);
-  
-    pst.setString(1, coitemname);
-    pst.setString(2, cocateg);
-   
 
-    pst.addBatch();
-        }
-pst.executeBatch();
-conn.commit();
-}
-catch(Exception e){
-    JOptionPane.showMessageDialog(this,e.getMessage());
-}
-        */
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
-        try{
-
-int rows=tblCO2.getRowCount();
-Class.forName("com.mysql.jdbc.Driver").newInstance();
-conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/iotams",user,pass);
-conn.setAutoCommit(false);
-
-String queryco = "Insert into tblcheckout(CheckoutID,ItemTagNo,ItemName,Category,Description,Location) values (?,?,?,?,?)";
-pst = conn.prepareStatement(queryco);
-for(int row = 0; row<rows; row++)
-{
-        
-        
-          jTextField1.setText(model.getValueAt(i,0).toString());
-    String coitemname = (String)tblCO2.getValueAt(row, 0);
-    String cocateg = (String)tblCO2.getValueAt(row, 1);
-    String codesc = (String)tblCO2.getValueAt(row, 2);
-    String coloc = (String)tblCO2.getValueAt(row, 3);
-    String coitemtagno = (String)tblCO2.getValueAt(row, 4);
-    pst.setString(1, coitemname);
-    pst.setString(2, cocateg);
-    pst.setString(3, codesc);
-    pst.setString(4, coloc);
-    pst.setString(5, coitemtagno);
-
-    pst.addBatch();
-}
-pst.executeBatch();
-con.commit();
-}
-catch(Exception e){
-    JOptionPane.showMessageDialog(this,e.getMessage());
-}
-        */
-        
-        
-        
-        
-        
-        
-        
-        
-        
         //FOUND A CODE WHERE YOU ARE ABLE TO ADD MANY ROWS IN DATABASE FASTER
         
         
