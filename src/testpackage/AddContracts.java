d7ab64822c3da7de97984fff8185904534d8c4f4
Christos <iChristos_@hotmail.com>
2017-02-17 22:28:50 +0200

    Contracts

--- src/testpackage/AddContracts.java
+++ src/testpackage/AddContracts.java
@@ -431,7 +431,7 @@ contract = new Contract(rs.getInt("ID_contract"),rs.getString("fk_ID_researcher"
                 jButton3ActionPerformed(evt);
             }
         });
-        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1120, 550, 203, 50));
+        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1120, 540, 203, 50));
         jPanel2.add(jComboBox3, new org.netbeans.lib.awtextra.AbsoluteConstraints(138, 273, 398, -1));
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
@@ -540,7 +540,7 @@ contract = new Contract(rs.getInt("ID_contract"),rs.getString("fk_ID_researcher"
                 jTextFieldSearchKeyTyped(evt);
             }
         });
-        jPanel2.add(jTextFieldSearch, new org.netbeans.lib.awtextra.AbsoluteConstraints(1120, 430, 200, 50));
+        jPanel2.add(jTextFieldSearch, new org.netbeans.lib.awtextra.AbsoluteConstraints(1120, 430, 200, 40));
 
         jButtonSearch.setBackground(new java.awt.Color(255, 204, 153));
         jButtonSearch.setFont(new java.awt.Font("Helvetica", 1, 16)); // NOI18N
@@ -550,7 +550,7 @@ contract = new Contract(rs.getInt("ID_contract"),rs.getString("fk_ID_researcher"
                 jButtonSearchActionPerformed(evt);
             }
         });
-        jPanel2.add(jButtonSearch, new org.netbeans.lib.awtextra.AbsoluteConstraints(1120, 490, 200, 50));
+        jPanel2.add(jButtonSearch, new org.netbeans.lib.awtextra.AbsoluteConstraints(1120, 480, 200, 50));
 
         jButton11.setBackground(new java.awt.Color(255, 204, 153));
         jButton11.setFont(new java.awt.Font("Helvetica", 1, 16)); // NOI18N
